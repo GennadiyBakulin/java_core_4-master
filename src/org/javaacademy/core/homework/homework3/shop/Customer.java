e3375c619b22666d26fc8337db1b5048f12a5249
Gennadiy Bakulin <g.bakulin@yandex.ru>
2024-08-09 18:39:30 +0700

    create ex1 out Homework3

--- src/org/javaacademy/core/homework/homework3/shop/Customer.java
+++ src/org/javaacademy/core/homework/homework3/shop/Customer.java
@@ -4,14 +4,21 @@ package org.javaacademy.core.homework.homework3.shop;
  * Покупатель
  */
 public class Customer {
-    String name;
 
-    void checkShop(Shop shop) {
-        //ПОМЕНЯТЬ ЗДЕСЬ ВЫРАЖЕНИЕ
-        if (true) {
-            System.out.println(name + ": Магазин прошел проверку");
-        } else {
-            System.out.println(name + ": Магазин не прошел проверку!");
-        }
+  private String name;
+
+  public Customer(String name) {
+    this.name = name;
+  }
+
+  void checkShop(Shop shop) {
+    //ПОМЕНЯТЬ ЗДЕСЬ ВЫРАЖЕНИЕ
+    if (shop.isClearFloor() && shop.getCountWorkers() > 2
+        || shop.isClearFloor() && shop.isWorkersHasBadge()
+        || shop.getCountWorkers() > 2 && shop.isWorkersHasBadge()) {
+      System.out.println(name + ": Магазин прошел проверку");
+    } else {
+      System.out.println(name + ": Магазин не прошел проверку!");
     }
+  }
 }
